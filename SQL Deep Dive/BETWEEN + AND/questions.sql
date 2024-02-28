a57278ed3e12e5e79349e1e608a65e2c14db0b17
pkalinska <48394065+pkalinska@users.noreply.github.com>
2024-02-28 13:07:43 +0100

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -4,8 +4,16 @@
 /*
 * Write your query here
 */
+select * 
+from customers
+WHERE age BETWEEN 30 ANd 50 and income < 50000;
+
+
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
 */
+select avg(income)
+from customers
+WHERE age BETWEEN 20 ANd 50;
